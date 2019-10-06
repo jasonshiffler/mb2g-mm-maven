7f4d7a0808279c66c14d3a9fa425f9b8cddc73e2
Jason Shiffler <jasonshiffler@yahoo.com>
2019-10-06 15:23:46 -0500

    initial commit

---
+++ web-app/src/main/java/com/shiffler/controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.shiffler.controllers;
+
+import com.shiffler.domain.UserCommand;
+import com.shiffler.entities.User;
+
+public class UserController {
+
+    User saveUser(UserCommand command){
+        return new User();
+    }
+
+}
